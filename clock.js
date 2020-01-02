86730f0af3bdfe7d9db04a8798f7ccbefc5d4838
PersonableD <58393789+PersonableD@users.noreply.github.com>
2020-01-03 00:15:14 +0900

    Update clock.js
    minutes 10분 이하 표시가 한자리인 것을 2자리로 표시되게 수정

--- clock.js
+++ clock.js
@@ -4,13 +4,9 @@ function getTime() {
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
-<<<<<<< HEAD
-  clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${minutes}`;
-=======
   clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }`;
->>>>>>> master
 }
 
 function init() {
